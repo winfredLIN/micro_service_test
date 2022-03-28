3e965a63eb2cf12f5b14c16fca0853897171b4ec
naonao <linxiaotao1998@live.com>
2022-03-28 19:47:27 +0800

    add stock client

--- services/stockService/client.go
+++ services/stockService/client.go
@@ -1,1 +1,34 @@
 package stockService
+
+import (
+	pb "api/protobuf/stock"
+	cfg "config"
+	"context"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// 配置客户端
+type Config struct {
+	Host string
+	Port string
+}
+
+var config = Config{
+	Host: cfg.GetConfig().Client.Host,
+	Port: ":" + cfg.GetConfig().Client.Port,
+}
+
+func NewStockServiceClient() (pb.StockServiceClient, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	connection, err := grpc.DialContext(ctx, config.Host+config.Port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		return nil, err
+	}
+	return pb.NewStockServiceClient(connection), nil
+}
